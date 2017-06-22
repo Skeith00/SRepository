8eed65c54c43cd636ed5ae98738b336fa1f5ea73
Sergi <smdt505@hotmail.com>
2017-06-22 23:41:52 +0200

    Eliminated two projects

--- knowledgeTest/src/main/java/com/rocketroi/knowledgetest/com/rocketroi/knowledgetest/bean/Diet.java
+++
@@ -1,5 +0,0 @@
-package main.java.com.rocketroi.knowledgetest.com.rocketroi.knowledgetest.bean;
-
-public enum Diet {
-		Cereales, Pienso, Pan, Jamón	
-}
--- knowledgeTest/src/main/java/com/rocketroi/knowledgetest/com/rocketroi/knowledgetest/bean/Size.java
+++
@@ -1,5 +0,0 @@
-package main.java.com.rocketroi.knowledgetest.com.rocketroi.knowledgetest.bean;
-
-public enum Size {
-		XS, S, M, L , XL
-}
